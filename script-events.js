e0aa3dd604254c59a5dec1e8cd03af196c05aa32
coethan04 <ethanmco@gmail.com>
2026-03-02 23:16:54 -0500

    Update map access token and data sources

--- script-events.js
+++ script-events.js
@@ -1,18 +1,16 @@
-/*--------------------------------------------------------------------
-GGR472 WEEK 6: JavaScript for Web Maps
-MapData and MapMouse Events
---------------------------------------------------------------------*/
-
-
 // Define access token
-mapboxgl.accessToken = '...'; //*** ADD PUBLIC ACCESS TOKEN HERE ***
+mapboxgl.accessToken = 'pk.eyJ1IjoiY29ldGhhbiIsImEiOiJjbW04Mm9vNTAwem5hMnFwbXA3bm9sYzg1In0.FUDLFtuUAAp3eF1BSszV6g'; //*** ADD PUBLIC ACCESS TOKEN HERE ***
 
 // Initialize map
 const map = new mapboxgl.Map({
     container: 'my-map',
-    style: 'mapbox://styles/mapbox/light-v11',
-    center: [-95, 63],
+    style: 'mapbox://styles/coethan/cmm9gnlfw001401s1ddxv60r3',
+    center: [-105, 58],
     zoom: 3,
+    maxbounds: [
+        [-180, 30],
+        [-25,84]
+    ],
     minZoom: 3
 });
 
@@ -31,15 +29,15 @@ map.addControl(new mapboxgl.NavigationControl());
 
 // Add data source and draw initial visiualization of layer
 map.on('load', () => {
-    map.addSource('canada-provterr', {
+    map.addSource('canada-country', {
         'type': 'geojson',
-        'data': 'https://raw.githubusercontent.com/smith-lg/ggr472-wk6-demo/main/data/can-provterr.geojson'
+        'data': 'https://raw.githubusercontent.com/coethan04/Lab3GGR472/refs/heads/main/map-3.geojson'
     });
 
     map.addLayer({
-        'id': 'canada-provterr-fill',
+        'id': 'canada-country-fill',
         'type': 'fill',
-        'source': 'canada-provterr',
+        'source': 'canada-country',
         'paint': {
             'fill-color': '#627BC1',
             'fill-opacity': 0.5,
@@ -49,9 +47,9 @@ map.on('load', () => {
 
     // Add another visualization of the polygon of provinces. Note we do not use addsource again
     map.addLayer({
-        'id': 'canada-provterr-hl', // Update id to represent highlighted layer
+        'id': 'canada-country-hl', // Update id to represent highlighted layer
         'type': 'fill',
-        'source': 'canada-provterr',
+        'source': 'canada-country',
         'paint': {
             'fill-color': '#627BC1',
             'fill-opacity': 1, // Opacity set to 1
@@ -67,13 +65,13 @@ map.on('load', () => {
 /*--------------------------------------------------------------------
 SIMPLE CLICK EVENT
 --------------------------------------------------------------------*/
-// map.on('click', 'canada-provterr-fill', (e) => {
+// map.on('click', 'canada-country-fill', (e) => {
 
 //     console.log(e);     // e is the event info triggered and is passed to the function as a parameter (e)
-//                         // Explore console output using Google DevTools
+//                         Explore console output using Google DevTools
 
-//     // let provname = e.features[0].properties.PRENAME;
-//     // console.log(provname);
+//     let provname = e.features[0].properties.PRENAME;
+//     console.log(provname);
 
 // });
 
@@ -81,33 +79,33 @@ SIMPLE CLICK EVENT
 /*--------------------------------------------------------------------
 ADD POP-UP ON CLICK EVENT
 --------------------------------------------------------------------*/
-// map.on('mouseenter', 'canada-provterr-fill', () => {
-//     map.getCanvas().style.cursor = 'pointer'; // Switch cursor to pointer when mouse is over provterr-fill layer
-// });
+map.on('mouseenter', 'canada-country-fill', () => {
+    map.getCanvas().style.cursor = 'pointer'; // Switch cursor to pointer when mouse is over provterr-fill layer
+});
 
-// map.on('mouseleave', 'canada-provterr-fill', () => {
-//     map.getCanvas().style.cursor = ''; // Switch cursor back when mouse leaves provterr-fill layer
-//     map.setFilter("canada-provterr-hl", ['==', ['get', 'PRUID'], '']); // Reset filter for highlighted layer after mouse leaves feature
-// });
+map.on('mouseleave', 'canada-country-fill', () => {
+    map.getCanvas().style.cursor = ''; // Switch cursor back when mouse leaves provterr-fill layer
+    map.setFilter("canada-provterr-hl", ['==', ['get', 'PRUID'], '']); // Reset filter for highlighted layer after mouse leaves feature
+});
 
 
-// map.on('click', 'canada-provterr-fill', (e) => {
-//     new mapboxgl.Popup() // Declare new popup object on each click
-//         .setLngLat(e.lngLat) // Use method to set coordinates of popup based on mouse click location
-//         .setHTML("<b>Province/Territory:</b> " + e.features[0].properties.PRENAME + "<br>" +
-//             "Population: " + e.features[0].properties.POP2021) // Use click event properties to write text for popup
-//         .addTo(map); // Show popup on map
-// });
+map.on('click', 'canada-country-fill', (e) => {
+    new mapboxgl.Popup() // Declare new popup object on each click
+        .setLngLat(e.lngLat) // Use method to set coordinates of popup based on mouse click location
+        .setHTML("<b>Province/Territory:</b> " + e.features[0].properties.PRENAME + "<br>" +
+            "Population: " + e.features[0].properties.POP2021) // Use click event properties to write text for popup
+        .addTo(map); // Show popup on map
+});
 
 
 /*--------------------------------------------------------------------
 SIMPLE HOVER EVENT
 // --------------------------------------------------------------------*/
-// map.on('mousemove', 'canada-provterr-fill', (e) => {
+map.on('mousemove', 'canada-country-fill', (e) => {
 
-//     // Set the filter of the provinces-hl to display the feature you're hovering over
-//     // e.features[0] is the first feature in the array and properties.PRUID is the Province ID for that feature
-//     map.setFilter('canada-provterr-hl', ['==', ['get', 'PRUID'], e.features[0].properties.PRUID]);
+    // Set the filter of the provinces-hl to display the feature you're hovering over
+    // e.features[0] is the first feature in the array and properties.PRUID is the Province ID for that feature
+    map.setFilter('canada-country-hl', ['==', ['get', 'PRUID'], e.features[0].properties.PRUID]);
 
-// });
+});
 
