716b8920ae2990550d09c2abeca36f5575e2f823
coethan04 <ethanmco@gmail.com>
2026-03-02 23:16:05 -0500

    Update Canada map source and layer identifiers

--- script-events-adv.js
+++ script-events-adv.js
@@ -1,16 +1,11 @@
-/*--------------------------------------------------------------------
-GGR472 WEEK 6: JavaScript for Web Maps
-MapData and MapMouse Events
---------------------------------------------------------------------*/
-
 
 // Define access token
-mapboxgl.accessToken = '...'; //***ADD PUBLIC ACCESS TOKEN*** 
+mapboxgl.accessToken = 'pk.eyJ1IjoiY29ldGhhbiIsImEiOiJjbW04Mm9vNTAwem5hMnFwbXA3bm9sYzg1In0.FUDLFtuUAAp3eF1BSszV6g'; //***ADD PUBLIC ACCESS TOKEN*** 
 
 // Initialize map
 const map = new mapboxgl.Map({
     container: 'my-map',
-    style: 'mapbox://styles/mapbox/light-v11',
+    style: 'mapbox://styles/coethan/cmm9gnlfw001401s1ddxv60r3',
     center: [-95, 63],
     zoom: 3,
     minZoom: 3
@@ -34,17 +29,17 @@ map.addControl(new mapboxgl.NavigationControl());
 map.on('load', () => {
 
     // Use GeoJSON file as vector tile creates non-unique IDs for features which causes difficulty when highlighting polygons
-    map.addSource('canada-provterr', {
+    map.addSource('canada-country', {
         'type': 'geojson',
-        'data': 'https://raw.githubusercontent.com/smith-lg/ggr472-wk6-demo/main/data/can-provterr.geojson', // Link to raw github files when in development stage. Update to pages on deployment
+        'data': 'https://raw.githubusercontent.com/coethan04/Lab3GGR472/refs/heads/main/map-3.geojson', // Link to raw github files when in development stage. Update to pages on deployment
         'generateId': true // Create a unique ID for each feature
     });
 
     // Add layer only once using case expression and feature state for opacity
     map.addLayer({
-        'id': 'canada-provterr-fill',
+        'id': 'canada-country-fill',
         'type': 'fill',
-        'source': 'canada-provterr',
+        'source': 'canada-country',
         'paint': {
             'fill-color': '#627BC1',
             'fill-opacity': [
@@ -65,10 +60,10 @@ map.on('load', () => {
 /*--------------------------------------------------------------------
 SIMPLE CLICK EVENT
 --------------------------------------------------------------------*/
-// map.on('click', 'canada-provterr-fill', (e) => {
+// map.on('click', 'canada-country-fill', (e) => {
 
-//     //console.log(e);    // e is the event info triggered and is passed to the function as a parameter (e)
-//                          // Explore console output using Google DevTools
+//     console.log(e);    // e is the event info triggered and is passed to the function as a parameter (e)
+//     Explore console output using Google DevTools
 
 //     let provname = e.features[0].properties.PRENAME;
 //     console.log(provname);
@@ -79,16 +74,16 @@ SIMPLE CLICK EVENT
 /*--------------------------------------------------------------------
 ADD POP-UP ON CLICK EVENT
 --------------------------------------------------------------------*/
-map.on('mouseenter', 'canada-provterr-fill', () => {
+map.on('mouseenter', 'canada-country-fill', () => {
     map.getCanvas().style.cursor = 'pointer'; // Switch cursor to pointer when mouse is over provterr-fill layer
 });
 
-map.on('mouseleave', 'canada-provterr-fill', () => {
+map.on('mouseleave', 'canada-country-fill', () => {
     map.getCanvas().style.cursor = ''; // Switch cursor back when mouse leaves provterr-fill layer
 });
 
 
-map.on('click', 'canada-provterr-fill', (e) => {
+map.on('click', 'canada-country-fill', (e) => {
     new mapboxgl.Popup() // Declare new popup object on each click
         .setLngLat(e.lngLat) // Use method to set coordinates of popup based on mouse click location
         .setHTML("<b>Province/Territory:</b> " + e.features[0].properties.PRENAME + "<br>" +
@@ -103,30 +98,31 @@ HOVER EVENT
 // --------------------------------------------------------------------*/
 let provID = null; // Declare initial province ID as null
 
-map.on('mousemove', 'canada-provterr-fill', (e) => {
-
-    // Set hover feature state back to false to remove opacity from previous highlighted polygon
-    map.setFeatureState(
-        { source: 'canada-provterr', id: provID },
-        { hover: false }
-    );
+map.on('mousemove', 'canada-country', (e) => {
+    if (e.features.length > 0)
+        // Set hover feature state back to false to remove opacity from previous highlighted polygon
+        if (provID !== null)
+            map.setFeatureState(
+                { source: 'canada-country', id: provID },
+                { hover: false }
+            );
 
     provID = e.features[0].id; // Update provID to featureID
     map.setFeatureState(
-        { source: 'canada-provterr', id: provID },
+        { source: 'canada-country', id: provID },
         { hover: true } // Update hover feature state to TRUE to change opacity of layer to 1
     );
 
 });
 
 // If mouse leaves the geojson layer, set all hover states to false and provID variable back to null
-map.on('mouseleave', 'canada-provterr-fill', () => { 
-
-    map.setFeatureState(
-        { source: 'canada-provterr', id: provID },
-        { hover: false }
-    );
-
+map.on('mouseleave', 'canada-country-fill', () => {
+    if (provID !== null) {
+        map.setFeatureState(
+            { source: 'canada-country', id: provID },
+            { hover: false }
+        );
+    }
     provID = null;
 });
 
